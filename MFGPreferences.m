b21d55d679e7f357a4a974d3f4fde090d6753170
majiqian5 <106007992+majiqian5@users.noreply.github.com>
2026-08-10 00:25:45 +0800

    Refactor MFGPreferences initialization and defaults
    Refactor MFGPreferences to set defaults on initialization and reload preferences from plist.

--- MFGPreferences.m
+++ MFGPreferences.m
@@ -7,7 +7,6 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         instance = [[MFGPreferences alloc] init];
-        [instance reloadPreferences];
     });
     return instance;
 }
@@ -15,118 +14,104 @@
 - (instancetype)init {
     self = [super init];
     if (self) {
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(reloadPreferences)
-                                                     name:@"musicfg/preferencesChanged"
-                                                   object:nil];
+        [self setDefaults];
     }
     return self;
 }
 
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
-- (UIColor *)colorFromHexString:(NSString *)hexString {
-    if (!hexString || hexString.length == 0) return nil;
-    
-    NSString *cleanString = [hexString stringByReplacingOccurrencesOfString:@"#" withString:@""];
-    if (cleanString.length != 6) return nil;
-    
-    unsigned int rgbValue = 0;
-    NSScanner *scanner = [NSScanner scannerWithString:cleanString];
-    [scanner scanHexInt:&rgbValue];
-    
-    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0
-                           green:((rgbValue & 0x00FF00) >> 8)/255.0
-                            blue:(rgbValue & 0x0000FF)/255.0
-                           alpha:1.0];
-}
-
-- (NSArray *)parseColorPresets:(NSString *)presetString {
-    if (!presetString || presetString.length == 0) return nil;
-    
-    NSArray *colors = [presetString componentsSeparatedByString:@","];
-    NSMutableArray *result = [NSMutableArray array];
-    
-    for (NSString *colorStr in colors) {
-        NSString *trimmed = [colorStr stringByTrimmingCharactersInSet:
-                             [NSCharacterSet whitespaceCharacterSet]];
-        UIColor *color = [self colorFromHexString:trimmed];
-        if (color) [result addObject:color];
-    }
-    
-    return result.count > 0 ? result : nil;
+- (void)setDefaults {
+    _enabled = YES;
+    _playerSizeScale = 1.0;
+    _playerPositionY = 0;
+    _cornerRadius = 16;
+    _borderWidth = 0;
+    _borderColor = [UIColor whiteColor];
+    _borderColorAnimation = NO;
+    _shadowOffsetY = 0;
+    _shadowRadius = 0;
+    _shadowColor = [UIColor blackColor];
+    _shadowColorAnimation = NO;
+    _shadowAnimationSpeed = 1.0;
+    _fontSizeScale = 1.0;
+    _fontColor = [UIColor whiteColor];
+    _rainbowTextEnabled = NO;
+    _timeFontSize = 14;
+    _timeColor = [UIColor whiteColor];
+    _progressHeight = 3;
+    _progressColor = [UIColor systemBlueColor];
+    _rainbowProgressEnabled = NO;
+    _glowEnabled = NO;
+    _glowStyle = 0;
+    _glowWidth = 3;
+    _glowColor = [UIColor systemBlueColor];
+    _glowSpeed = 1.0;
+    _spectrumEnabled = NO;
+    _spectrumHeight = 40;
+    _spectrumBarCount = 32;
+    _spectrumBarSpacing = 2;
+    _spectrumColor = [UIColor systemBlueColor];
+    _spectrumRainbowEnabled = NO;
+    _rainbowSpeed = 1.0;
+    _notificationEnabled = NO;
+    _notificationCornerRadius = 16;
+    _notificationBorderWidth = 0;
+    _notificationBorderColor = [UIColor whiteColor];
+    _notificationShadowRadius = 0;
+    _notificationShadowColor = [UIColor blackColor];
+    _platterShadowOffsetY = 0;
+    _platterShadowRadius = 0;
+    _platterShadowAnimationSpeed = 1.0;
 }
 
 - (void)reloadPreferences {
-    NSDictionary *prefs = [[NSDictionary alloc] initWithContentsOfFile:
-                           @"/var/mobile/Library/Preferences/musicfg.plist"];
+    NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:
+        @"/var/mobile/Library/Preferences/musicfg.plist"];
+    if (!prefs) return;
     
-    // 总开关
-    self.enabled = [prefs[@"EnableNotificationEffect"] boolValue] ?: YES;
+    if (prefs[@"Enabled"]) _enabled = [prefs[@"Enabled"] boolValue];
+    if (prefs[@"PlayerSizeScale"]) _playerSizeScale = [prefs[@"PlayerSizeScale"] floatValue];
+    if (prefs[@"PlayerPositionY"]) _playerPositionY = [prefs[@"PlayerPositionY"] floatValue];
+    if (prefs[@"CornerRadius"]) _cornerRadius = [prefs[@"CornerRadius"] floatValue];
+    if (prefs[@"BorderWidth"]) _borderWidth = [prefs[@"BorderWidth"] floatValue];
+    if (prefs[@"BorderColor"]) _borderColor = [self colorFromHex:prefs[@"BorderColor"]];
+    if (prefs[@"BorderColorAnimation"]) _borderColorAnimation = [prefs[@"BorderColorAnimation"] boolValue];
+    if (prefs[@"ShadowOffsetY"]) _shadowOffsetY = [prefs[@"ShadowOffsetY"] floatValue];
+    if (prefs[@"ShadowRadius"]) _shadowRadius = [prefs[@"ShadowRadius"] floatValue];
+    if (prefs[@"ShadowColor"]) _shadowColor = [self colorFromHex:prefs[@"ShadowColor"]];
+    if (prefs[@"ShadowColorAnimation"]) _shadowColorAnimation = [prefs[@"ShadowColorAnimation"] boolValue];
+    if (prefs[@"FontSizeScale"]) _fontSizeScale = [prefs[@"FontSizeScale"] floatValue];
+    if (prefs[@"FontColor"]) _fontColor = [self colorFromHex:prefs[@"FontColor"]];
+    if (prefs[@"RainbowTextEnabled"]) _rainbowTextEnabled = [prefs[@"RainbowTextEnabled"] boolValue];
+    if (prefs[@"ProgressHeight"]) _progressHeight = [prefs[@"ProgressHeight"] floatValue];
+    if (prefs[@"ProgressColor"]) _progressColor = [self colorFromHex:prefs[@"ProgressColor"]];
+    if (prefs[@"RainbowProgressEnabled"]) _rainbowProgressEnabled = [prefs[@"RainbowProgressEnabled"] boolValue];
+    if (prefs[@"GlowEnabled"]) _glowEnabled = [prefs[@"GlowEnabled"] boolValue];
+    if (prefs[@"GlowStyle"]) _glowStyle = [prefs[@"GlowStyle"] integerValue];
+    if (prefs[@"GlowWidth"]) _glowWidth = [prefs[@"GlowWidth"] floatValue];
+    if (prefs[@"GlowColor"]) _glowColor = [self colorFromHex:prefs[@"GlowColor"]];
+    if (prefs[@"GlowSpeed"]) _glowSpeed = [prefs[@"GlowSpeed"] floatValue];
+    if (prefs[@"SpectrumEnabled"]) _spectrumEnabled = [prefs[@"SpectrumEnabled"] boolValue];
+    if (prefs[@"SpectrumHeight"]) _spectrumHeight = [prefs[@"SpectrumHeight"] floatValue];
+    if (prefs[@"SpectrumBarCount"]) _spectrumBarCount = [prefs[@"SpectrumBarCount"] integerValue];
+    if (prefs[@"SpectrumBarSpacing"]) _spectrumBarSpacing = [prefs[@"SpectrumBarSpacing"] floatValue];
+    if (prefs[@"SpectrumColor"]) _spectrumColor = [self colorFromHex:prefs[@"SpectrumColor"]];
+    if (prefs[@"SpectrumRainbowEnabled"]) _spectrumRainbowEnabled = [prefs[@"SpectrumRainbowEnabled"] boolValue];
+    if (prefs[@"RainbowSpeed"]) _rainbowSpeed = [prefs[@"RainbowSpeed"] floatValue];
+}
+
+- (UIColor *)colorFromHex:(NSString *)hex {
+    if (!hex || hex.length == 0) return [UIColor whiteColor];
     
-    // 播放器大小与位置
-    self.playerSizeScale = [prefs[@"PlayerSizeScale"] floatValue] ?: 1.0;
-    self.playerPositionY = [prefs[@"PlayerPositionY"] floatValue] ?: 0.0;
+    NSString *cleanHex = [hex stringByReplacingOccurrencesOfString:@"#" withString:@""];
+    unsigned int rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:cleanHex];
+    [scanner scanHexInt:&rgbValue];
     
-    // 圆角
-    self.cornerRadius = [prefs[@"CornerRadius"] floatValue] ?: 22.0;
+    CGFloat r = ((rgbValue & 0xFF0000) >> 16) / 255.0;
+    CGFloat g = ((rgbValue & 0x00FF00) >> 8) / 255.0;
+    CGFloat b = (rgbValue & 0x0000FF) / 255.0;
     
-    // 边框
-    self.borderWidth = [prefs[@"NotificationBorderWidth"] floatValue] ?: 2.0;
-    self.borderColor = [self colorFromHexString:prefs[@"BorderColor"]];
-    self.borderColorAnimation = [prefs[@"BorderColorAnimation"] boolValue] ?: YES;
-    
-    // 阴影
-    self.shadowOffsetY = [prefs[@"NotificationShadowOffsetY"] floatValue] ?: 3.0;
-    self.shadowRadius = [prefs[@"NotificationShadowRadius"] floatValue] ?: 5.0;
-    self.shadowColor = [self colorFromHexString:prefs[@"ShadowColor"]];
-    self.shadowColorAnimation = [prefs[@"ShadowColorAnimation"] boolValue] ?: YES;
-    self.shadowAnimationSpeed = [prefs[@"NotificationShadowAnimationSpeed"] floatValue] ?: 3.0;
-    
-    // 字体设置
-    self.fontSizeScale = [prefs[@"FontSizeScale"] floatValue] ?: 1.0;
-    self.fontColor = [self colorFromHexString:prefs[@"FontColor"]];
-    self.rainbowTextEnabled = [prefs[@"RainbowTextEnabled"] boolValue] ?: NO;
-    
-    // 时间标签
-    self.timeFontSize = [prefs[@"TimeFontSize"] floatValue] ?: 14.0;
-    self.timeColor = [self colorFromHexString:prefs[@"TimeColor"]];
-    
-    // 进度条
-    self.progressHeight = [prefs[@"ProgressHeight"] floatValue] ?: 4.0;
-    self.progressColor = [self colorFromHexString:prefs[@"ProgressColor"]];
-    self.rainbowProgressEnabled = [prefs[@"RainbowProgressEnabled"] boolValue] ?: NO;
-    
-    // 灵动光圈
-    self.glowEnabled = [prefs[@"GlowEnabled"] boolValue] ?: YES;
-    self.glowStyle = [prefs[@"GlowStyle"] integerValue] ?: 0;
-    self.glowWidth = [prefs[@"GlowWidth"] floatValue] ?: 3.0;
-    self.glowColor = [self colorFromHexString:prefs[@"GlowColor"]] ?: [UIColor systemBlueColor];
-    self.glowSpeed = [prefs[@"GlowSpeed"] floatValue] ?: 1.0;
-    
-    // 频谱可视化
-    self.spectrumEnabled = [prefs[@"SpectrumEnabled"] boolValue] ?: NO;
-    self.spectrumHeight = [prefs[@"SpectrumHeight"] floatValue] ?: 40.0;
-    self.spectrumBarCount = [prefs[@"SpectrumBarCount"] integerValue] ?: 32;
-    self.spectrumBarSpacing = [prefs[@"SpectrumBarSpacing"] floatValue] ?: 2.0;
-    self.spectrumColor = [self colorFromHexString:prefs[@"SpectrumColor"]] ?: [UIColor systemBlueColor];
-    self.spectrumRainbowEnabled = [prefs[@"SpectrumRainbowEnabled"] boolValue] ?: NO;
-    
-    // 彩虹渐变速度
-    self.rainbowSpeed = [prefs[@"RainbowSpeed"] floatValue] ?: 1.0;
-    
-    // 颜色预设
-    self.colorPresets = [self parseColorPresets:prefs[@"ColorPresets"]];
-    
-    // 通知效果
-    self.platterEnabled = [prefs[@"EnablePlatterEffect"] boolValue] ?: YES;
-    self.platterCornerRadius = [prefs[@"PlatterCornerRadius"] floatValue] ?: 22.0;
-    self.platterBorderWidth = [prefs[@"PlatterBorderWidth"] floatValue] ?: 2.0;
-    self.platterShadowOffsetY = [prefs[@"PlatterShadowOffsetY"] floatValue] ?: 3.0;
-    self.platterShadowRadius = [prefs[@"PlatterShadowRadius"] floatValue] ?: 5.0;
-    self.platterShadowAnimationSpeed = [prefs[@"PlatterShadowAnimationSpeed"] floatValue] ?: 3.0;
+    return [UIColor colorWithRed:r green:g blue:b alpha:1.0];
 }
 
 @end
