d58aa17c08a9e857916da83252b6e8d01df3bd50
majiqian5 <106007992+majiqian5@users.noreply.github.com>
2026-08-10 04:21:55 +0800

    Add properties for shadow opacity and text styles

--- MFGPreferences.h
+++ MFGPreferences.h
@@ -5,11 +5,16 @@
 @property (nonatomic, assign) CGFloat cornerRadius;
 @property (nonatomic, assign) CGFloat borderWidth;
 @property (nonatomic, assign) CGFloat shadowRadius;
+@property (nonatomic, assign) CGFloat shadowOpacity;
 @property (nonatomic, assign) CGFloat fontScale;
 @property (nonatomic, assign) BOOL rainbowText;
+@property (nonatomic, assign) BOOL boldText;
 @property (nonatomic, assign) CGFloat rainbowSpeed;
+@property (nonatomic, assign) BOOL rainbowBorder;
+@property (nonatomic, assign) BOOL rainbowShadow;
 @property (nonatomic, assign) BOOL notificationEnabled;
 @property (nonatomic, assign) CGFloat notificationCornerRadius;
+@property (nonatomic, assign) CGFloat notificationBorderWidth;
 + (instancetype)sharedInstance;
 - (void)reloadPreferences;
 @end
