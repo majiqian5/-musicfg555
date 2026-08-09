d3fcb87607c0cbd8622f4a02d2b2619ba25983b2
majiqian5 <106007992+majiqian5@users.noreply.github.com>
2026-08-10 02:09:18 +0800

    Refactor MFGRootListController by removing methods
    Removed unused methods and cleaned up code.

--- preferences/MFGRootListController.m
+++ preferences/MFGRootListController.m
@@ -1,4 +1,3 @@
-#import <spawn.h>
 #import <Preferences/Preferences.h>
 
 @interface MFGRootListController : PSListController
@@ -7,37 +6,10 @@
 @implementation MFGRootListController
 
 - (NSArray *)specifiers {
-    if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
-    }
-    return _specifiers;
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"musicfg/preferencesChanged" object:nil];
-}
-
-- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
-    [super setPreferenceValue:value specifier:specifier];
-    
-    NSString *key = [specifier propertyForKey:@"key"];
-    if (key) {
-        NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithContentsOfFile:
-            @"/var/mobile/Library/Preferences/musicfg.plist"];
-        if (!prefs) prefs = [NSMutableDictionary dictionary];
-        prefs[key] = value;
-        [prefs writeToFile:@"/var/mobile/Library/Preferences/musicfg.plist" atomically:YES];
-    }
-    
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"musicfg/preferencesChanged" object:nil];
-}
-
-- (void)respring {
-    pid_t pid;
-    const char *args[] = {"killall", "SpringBoard", NULL};
-    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)args, NULL);
-    waitpid(pid, NULL, 0);
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+	return _specifiers;
 }
 
 @end
